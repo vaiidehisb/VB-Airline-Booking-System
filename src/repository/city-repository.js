a40f6a758e66ed59174ee726f0c41701be862728
vaidehiOBI <vaidehi@onebillionideas.io>
2025-07-20 10:46:32 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,22 +4,49 @@ class CityRepository {
     async createCity ({ name }) {
           try{
             const city = await City.create({name});
-            return city;
+            return City;
           } catch(error){
+                 console.log("Something went wrong in the repository layer");
                  throw{error};
           }
     }
 
-    async deleteCity ({name}){
+    async deleteCity ({cityId}){
         try {
             await City.destroy({
                  where: {
                     id: cityId,
                  }
             });
+            return true;
         }
         catch(error){
+            console.log("Something went wrong in the repository layer");
             throw{error};
+        }
+    }
+
+    async updateCity(cityId,data){
+        try {
+            const city = await City.updateCity(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return City;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try{
+            const city = await City.findOne();
+            return City;
+
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
         }
     }
 }
